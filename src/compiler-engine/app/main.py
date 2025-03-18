03d4b30906f0673cd60e2be21881ec1f997309cd
Enrico Cirignaco <enrico.cirignaco@students.bfh.ch>
2025-03-18 11:32:00 +0100

    get gilab repo

--- src/compiler-engine/app/main.py
+++ src/compiler-engine/app/main.py
@@ -3,11 +3,19 @@ from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from typing import List, Optional, Dict
 from enum import Enum
+import os
+import requests
+import docker
+from urllib.parse import quote
+import base64
+from urllib.parse import quote, urlparse
 
 app = FastAPI(
-    title="Compiler Engine - OpenAPI 3.1",
+    title="Compiler Engine",
     version="1.0",
 )
+GITLAB_API_URL = os.getenv("GITLAB_API_URL")
+gitlab_api_headers = {"PRIVATE-TOKEN": os.getenv("GROUP_ACCESS_TOKEN")}
 
 # Pydantic models for request bodies and responses
 
@@ -39,14 +47,14 @@ class Status(BaseModel):
 
 
 # Endpoints for standard compile
-
 @app.post("/compile", tags=["Compile standard"])
 async def compile_standard(request: StadardCompileRequest):
+    request.git_repo_url = convert_gilab_url(request.git_repo_url)
     # Business logic placeholder:
     # - Clone the repo from request.git_repo_url
     # - Use request.board, request.libraries, and request.custom_flags with the built-in toolchain (arduino-cli)
     # - Initiate a compile job and generate a job_id and timestamp.
-    return {"job_id": 10, "timestamp": "2021-10-10T10:10:10Z"}
+    return {"job_id": 10, "timestamp": "2021-10-10T10:10:10Z", "gitlab_url": request.git_repo_url}
 
 @app.get("/compile/{job_id}/status", tags=["Compile standard"])
 async def get_compile_status(job_id: int):
@@ -98,6 +106,22 @@ async def get_custom_compile_artefacts(job_id: int):
         return FileResponse(dummy_zip_path, media_type="application/zip", filename="artefacts.zip")
     except Exception:
         raise HTTPException(status_code=500, detail="Internal server error")
+    
+############################################################################################################
+def get_source_code(gitlab_url: str, ref: str = "main"):
+    encoded_repo_path = quote(gitlab_url.split("gitlab.ti.bfh.ch/")[1], safe="")
+    encoded_source_path = quote(os.getenv("DEFAULT_SOURCE_PATH"), safe="")
+    request_url = f"{GITLAB_API_URL}/{encoded_repo_path}/repository/files/{encoded_source_path}?ref={ref}"
+    request_url = f"{GITLAB_API_URL}/{encoded_repo_path}/repository/archive.zip?ref={ref}"
+    response = requests.get(request_url, headers=gitlab_api_headers)
+
+    if response.status_code == 200:
+        print("Repository archive downloaded successfully")
+        return response.content
+    else:
+        print("Error downloading repository archive:", response.text)
+        return None
+############################################################################################################
 
 
 if __name__ == "__main__":
