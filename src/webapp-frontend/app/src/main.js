e53febd01f6ea6b3541b9f191b6a07753091f3a2
Enrico Cirignaco <enrico.cirignaco@students.bfh.ch>
2025-04-10 07:01:51 +0000

    color palette fixed

--- src/webapp-frontend/app/src/main.js
+++ src/webapp-frontend/app/src/main.js
@@ -1,20 +1,11 @@
-    // Vuetify
-    import 'vuetify/styles'
-    import '@mdi/font/css/materialdesignicons.css'
-    import { createVuetify } from 'vuetify'
-    import * as components from 'vuetify/components'
-    import * as directives from 'vuetify/directives'
-
     import { createApp } from 'vue'
     import { createPinia } from 'pinia'
 
     import App from './App.vue'
     import router from './router'
+    // Import the Vuetify plugin configuration
+    import vuetify from './plugins/vuetify'
 
-    const vuetify = createVuetify({
-        components,
-        directives,
-        })
     const app = createApp(App)
 
     app.use(vuetify)
---
+++ src/webapp-frontend/app/src/plugins/vuetify.js
@@ -0,0 +1,32 @@
+// src/plugins/vuetify.js
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#F08700',
+          secondary: '#AA5302',
+          accent: '#F5C879',
+          background: '#FEFEFE',
+          surface: '#FDF6EB',
+        }
+      },
+      dark: {
+        primary: '#030100',
+        secondary: '#7B7653',
+        accent: '#344738',
+        background: '#F7B232',
+        surface: '#16332D',
+      }
+    },
+  },
+})
