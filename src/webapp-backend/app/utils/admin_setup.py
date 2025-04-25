1422ed8cc8e7b2b3c4bd201e209b8d52a3fa36a4
Linus Degen <linus.degen@students.bfh.ch>
2025-04-25 10:09:09 +0200

    Improve docker setup of backend. refs #31

--- src/webapp-backend/app/utils/admin_setup.py
+++ src/webapp-backend/app/utils/admin_setup.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from fastapi import HTTPException
 from ..services.auth_service import AuthService
 from ..utils.triplestore_client import TripleStoreClient
 from ..repositories.user_repository import UserRepository
@@ -12,24 +13,27 @@ def create_init_admin():
     admin_email = settings.INIT_ADMIN_MAIL
     admin_password = settings.INIT_ADMIN_PW or _generate_secure_password()
 
-    if not auth_service.find_user_email(admin_email):
-        admin = UserIn(
-            email=admin_email,
-            full_name="Init Admin",
-            password=admin_password,
-            role=RoleEnum.ADMIN
-        )
-        auth_service.create_user(admin)
-
-        border = "=" * 60
-        print(f"\n{border}")
-        print("Admin user has been created with the following credentials:")
-        print(f"Email:    {admin_email}")
-        print(f"Password: {admin_password}")
-        print("‼Please store this password securely and change it after first login.")
-        print(f"{border}\n")
-    else:
+    try:
+        auth_service.find_user_email(admin_email)
         print(f"Admin user with email {admin_email} already exists.")
+    # Service throws 404 if user not found
+    except HTTPException as error:
+        if(error.status_code == 404):
+            admin = UserIn(
+                email=admin_email,
+                full_name="Init Admin",
+                password=admin_password,
+                role=RoleEnum.ADMIN
+            )
+            auth_service.create_user(admin)
+
+            border = "=" * 60
+            print(f"\n{border}")
+            print("Admin user has been created with the following credentials:")
+            print(f"Email:    {admin_email}")
+            print(f"Password: {admin_password}")
+            print("‼Please store this password securely and change it after first login.")
+            print(f"{border}\n")
 
 def _generate_secure_password(length: int = 16) -> str:
     return secrets.token_urlsafe(length)
