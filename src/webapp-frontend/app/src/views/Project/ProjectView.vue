5a77620a715f13308066613815ddd6a75d9e8603
Enrico Cirignaco <enrico.cirignaco@students.bfh.ch>
2025-04-28 10:11:55 +0000

    bug back button fixed

--- src/webapp-frontend/app/src/views/Project/ProjectView.vue
+++ src/webapp-frontend/app/src/views/Project/ProjectView.vue
@@ -11,7 +11,8 @@
             <v-row class="w-100 align-center">
                 <!-- back button -->
                 <v-col cols="auto">
-                    <v-btn icon @click="$router.back()">
+                    <v-btn icon @click="router.push('/projects')">
+                        <v-icon>mdi-arrow-left</v-icon>
                     <v-icon>mdi-arrow-left</v-icon>
                     </v-btn>
                 </v-col>
