cf43b5de0144d26d8df5bc6d1014cf77b1a2363f
Enrico Cirignaco <enrico.cirignaco@students.bfh.ch>
2025-05-11 14:22:28 +0000

    sensor node implemented

--- src/webapp-frontend/app/src/views/SensorNode/SensorNodeView.vue
+++ src/webapp-frontend/app/src/views/SensorNode/SensorNodeView.vue
@@ -32,7 +32,7 @@
                   </v-chip>
                   <!-- edit button -->
                   <v-btn
-                  v-if="authStore.getUser?.role !== 'Researcher' && sensorNode.state.name !== 'unused'"
+                  v-if="authStore.getUser?.role !== 'Researcher'"
                   color="primary"
                   icon size="small"
                   class="me-1"
@@ -42,7 +42,7 @@
                   </v-btn>
                   <!-- delete button -->
                   <v-btn
-                  v-if="authStore.getUser?.role !== 'Researcher' && sensorNode.state.name !== 'unused'"
+                  v-if="authStore.getUser?.role !== 'Researcher' && sensorNode.state.name === 'Prepared'"
                   color="error"
                   icon size="small"
                   @click="deletesensorNode(sensorNodeId)"
@@ -60,28 +60,37 @@
               <p class="text-body-1">{{ sensorNode.description }}</p>
             </div>
             <v-list elevation="1" rounded="lg" density="comfortable">
-              <v-list-item style="min-height: 72px;">
+              <v-list-item
+              style="min-height: 72px; cursor: pointer;"
+              @click="openTTNUrl"
+              >
                 <template #prepend>
-                  <v-icon style="font-size: 32px;">mdi-source-repository</v-icon>
+                  <v-icon style="font-size: 32px;">mdi-web</v-icon>
                 </template>
-                <v-list-item-title>{{ sensorNode.gitlab_url }}</v-list-item-title>
-                <v-list-item-subtitle>GitLab URL</v-list-item-subtitle>
+                <v-list-item-title>{{ sensorNode.ttn_device_link }}</v-list-item-title>
+                <v-list-item-subtitle>The Things Network URL</v-list-item-subtitle>
               </v-list-item>
               <v-divider />
-              <v-list-item style="min-height: 72px;">
-                <template #prepend>
-                  <v-icon style="font-size: 32px;">mdi-source-branch</v-icon>
-                </template>
-                <v-list-item-title>{{ sensorNode.git_ref }}</v-list-item-title>
-                <v-list-item-subtitle>Git Reference</v-list-item-subtitle>
-              </v-list-item>
-              <v-divider />
-              <v-list-item style="min-height: 72px;">
+              <v-list-item
+              style="min-height: 72px; cursor: pointer;"
+              @click="router.push(`/project/${sensorNode.project_uuid}`)"
+              >
                 <template #prepend>
                   <v-icon style="font-size: 32px;">mdi-chip</v-icon>
                 </template>
-                <v-list-item-title>{{ sensorNode.hardware_type }}</v-list-item-title>
-                <v-list-item-subtitle>Hardware Type</v-list-item-subtitle>
+                <v-list-item-title>{{ sensorNode.project_uuid }}</v-list-item-title>
+                <v-list-item-subtitle>Project UUID</v-list-item-subtitle>
+              </v-list-item>
+              <v-divider />
+              <v-list-item
+              style="min-height: 72px; cursor: pointer;"
+              @click="router.push(`/node-template/${sensorNode.node_template_uuid}`)"
+              >
+                <template #prepend>
+                  <v-icon style="font-size: 32px;">mdi-chip</v-icon>
+                </template>
+                <v-list-item-title>{{ sensorNode.node_template_uuid }}</v-list-item-title>
+                <v-list-item-subtitle>Node Template UUID</v-list-item-subtitle>
               </v-list-item>
               <v-divider />
               <v-list-item style="min-height: 72px;">
@@ -98,7 +107,7 @@
               <h3 class="text-h6 mb-2">Configurables</h3>
               <v-list elevation="1" rounded="lg" density="comfortable">
                 <v-list-item
-                  v-for="(config, index) in sensorNode.configurables"
+                  v-for="(config, index) of sensorNode.configurables"
                   :key="index"
                   style="min-height: 72px;"
                   :style="config.type === 'SystemDefined' ? 'background-color: rgba(0,0,0,0.05); font-style: italic;' : ''"
@@ -106,15 +115,33 @@
                   <template #prepend>
                     <v-icon style="font-size: 28px;">mdi-cog</v-icon>
                   </template>
-                    <v-list-item-title>{{config.name}}: <strong>{{ config.value }}</strong></v-list-item-title>
+                  <v-list-item-title>{{ config.name }}: <strong>{{ config.value }}</strong></v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-col>
-            </v-row>
-            <!-- Fields Table -->
-            <h3 class="text-h6 mb-2 mt-6">Sensor node Fields</h3>
+            <!--  config header file preview -->
+            <v-col cols="6">
+              <h3 class="text-h6 mb-2">config.h Preview</h3>
+              <v-sheet
+              elevation="1"
+              class="pa-4 position-relative"
+              style="background-color: #272822; color: #f8f8f2; font-family: monospace; white-space: pre; overflow: auto; border-radius: 8px; max-height: 300px;"
+            >
+              {{ configPreview }}
+            </v-sheet>
+            </v-col>
+          </v-row>
+          </v-card-text>
+        </v-card>
+        <v-divider class="my-6" />
+        <!-- sensor data -->
+        <v-container v-if="sensorNode?.last_timeseries">
+          <h2 class="text-h6 mb-2"> Latest Sensor Data</h2>
+        <v-list-item-subtitle class="mt-n2 mb-4">
+          {{ new Date(sensorNode.last_timeseries.timestamp).toLocaleString(userLocale) }}
+        </v-list-item-subtitle>
             <v-data-table
-              :items="sensorNode.fields"
+              :items="sensorNode.last_timeseries.fields"
               :headers="fieldHeaders"
               density="compact"
               class="elevation-1 rounded-lg"
@@ -123,80 +150,10 @@
               elevation="1"
               disable-sort
             >
-            <template #item.commercial_sensor="{ item }">
-              <v-btn
-                variant="outlined"
-                color="secondary"
-                size="small"
-                rounded
-                :to="`/commercial-sensor/${item.commercial_sensor.uuid}`"
-                style="text-transform: none; min-width: 150px;"
-                
-              >
-                <v-icon start class="me-1">mdi-link-variant</v-icon>
-                {{ item.commercial_sensor.alias }}
-              </v-btn>
-            </template>
             </v-data-table>
-
-            <!-- protobuff schema -->
-            <v-row class="align-center mb-2 mt-6">
-              <v-col>
-                <h3 class="text-h6 mb-0">Protobuf Schema</h3>
-              </v-col>
-              <v-col cols="auto">
-                <v-btn
-                  color="secondary"
-                  size="small"
-                  :href="`/sensor-node/${sensorNodeId}/code`"
-                  download
-                  style="text-transform: none;"
-                >
-                  <v-icon start class="me-1">mdi-download</v-icon>
-                  Download Code
-                </v-btn>
-              </v-col>
-            </v-row>
-            <!-- <v-sheet
-              elevation="1"
-              class="pa-4 mt-4 position-relative"
-              style="background-color: #272822; color: #f8f8f2; font-family: monospace; white-space: pre; overflow: auto; border-radius: 8px; max-height: 300px;"
-            >
-              <v-btn
-                icon
-                size="small"
-                color="primary"
-                @click="copySchema"
-                class="position-absolute"
-                style="top: 8px; right: 8px;"
-              >
-                <v-icon>mdi-content-copy</v-icon>
-              </v-btn>
-              {{ protobuf_schema }}
-            </v-sheet> -->
-          </v-card-text>
-        </v-card>
-        <v-divider class="my-6" />
-        
-
-        <h2 class="text-h6 mb-2">Deployed Nodes</h2>
-        <!-- Table of deployed nodes
-        <v-data-table
-          :items="sensorNode.inherited_sensor_nodes"
-          :headers="sensorHeaders"
-          class="elevation-1 rounded-lg"
-          hover
-          density="compact"
-          rounded="lg"
-          elevation="1"
-        >
-          <template #item.state="{ item }">
-            <v-chip :color="grey" variant="flat" class="text-white" style="min-width: 80px; justify-content: center;">
-              {{ item.state }}
-            </v-chip>
-          </template>
-        </v-data-table> -->
+            </v-container>
       </v-col>
+      
       <!-- Logbook -->
       <v-col cols="3">
         <Logbook v-if="sensorNode?.logbook" :logbook="sensorNode.logbook" />
@@ -244,31 +201,28 @@ import sensorNodeService from '@/services/sensorNodeService'
 import Logbook from '@/components/Logbook.vue'
 import { useAuthStore } from '@/stores/authStore'
 import { useTextStore } from '@/stores/textStore'
+import { computed } from 'vue'
 
 const authStore = useAuthStore()
 const textStore = useTextStore()
-const sensorNodeId = ref(useRoute().params.id)
 const router = useRouter()
 const sensorNode = ref(null)
-
-const sensorHeaders = [
-  { title: 'Node ID', key: 'id' },
-  { title: 'Name', key: 'name'},
-  { title: 'Type', key: 'type' },
-  { title: 'Location', key: 'location' },
-  { title: 'State', key: 'state' },
-]
-const fieldHeaders = [
-  { title: 'Field Name', key: 'field_name' },
-  { title: 'Data Type', key: 'protbuf_datatype' },
-  { title: 'Unit', key: 'unit' },
-  { title: 'Commercial Sensor', key: 'commercial_sensor' },
-]
 const loading = ref(true)
 const confirmDelete = ref(false)
 const sensorNodeToDelete = ref(null)
 const deleteConfirmInput = ref('')
-const protobuf_schema = ref('')
+const sensorNodeId = ref(useRoute().params.id)
+const userLocale = typeof navigator !== 'undefined' ? navigator.language : 'en-US'
+const configPreview = computed(() => {
+  return sensorNode.value?.configurables?.map(c => `#define ${c.name} ${c.value}`).join('\n') || ''
+})
+const fieldHeaders = [
+  { title: 'Field Name', key: 'field_name' },
+  { title: 'Data Type', key: 'protobuf_datatype' },
+  { title: 'Value', key: 'value' },
+  { title: 'Unit', key: 'unit' },
+]
+
 // Fetch sensor node
 sensorNodeService.getSensorNode(sensorNodeId.value)
   .then((data) => {
@@ -282,25 +236,13 @@ sensorNodeService.getSensorNode(sensorNodeId.value)
       label: matchedState ? matchedState.label : data.state,
       color: matchedState ? matchedState.color : 'grey'
     }
-    console.log('sensor node', sensorNode.value)
+    // sort configurables by type
+    sensorNode.value.configurables.sort((a, b) => a.type.localeCompare(b.type))
   })
   .catch((error) => {
     console.error(`Error fetching sensor node ${sensorNodeId.value}:`, error)
   })
   .finally(() => loading.value = false)
-// Fetch protobuff schema
-// sensorNodeService.getSensorNodeSchema(sensorNodeId.value)
-//   .then((data) => {
-//     protobuf_schema.value = data
-//   })
-//   .catch((error) => {
-//     console.error(`Error fetching sensor node ${sensorNodeId.value} schema:`, error)
-//   })
-// // check if the sensor node has children
-// const hasInheritedSensorNodes = () => {
-//   return Array.isArray(sensorNode.inherited_sensor_nodes) && sensorNode.inherited_sensor_nodes.length > 0
-// }
-
 
 const deletesensorNode = (id) => {
   sensorNodeToDelete.value = id
@@ -316,18 +258,8 @@ const performDelete = () => {
     })
     .catch(() => console.error(`Error deleting sensor node ${sensorNodeToDelete.value}`))
 }
-const copySchema = () => {
-  const schemaText = JSON.stringify(protobuf_schema.value, null, 2)
-  navigator.clipboard.writeText(schemaText).catch(err => {
-    console.error('Failed to copy schema: ', err)
-  })
+
+const openTTNUrl = () => {
+    window.open(sensorNode.value.ttn_device_link, '_blank')
 }
-
-
-const latitude = 60.0
-const longitude = 24.0
-const zoom = 13
-
-const mapUrl = `https://www.openstreetmap.org/export/embed.html?bbox=${longitude - 0.05}%2C${latitude - 0.05}%2C${longitude + 0.05}%2C${latitude + 0.05}&layer=mapnik&marker=${latitude}%2C${longitude}`
-
 </script>
