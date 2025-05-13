03dfb2690eb2ac0a6d6d9456f7851dcffd97c4d3
Linus Degen <linus.degen@students.bfh.ch>
2025-05-13 14:07:01 +0200

    Implement update mechanics for sensor nodes. refs #56

--- src/webapp-backend/app/services/sensor_node_service.py
+++ src/webapp-backend/app/services/sensor_node_service.py
@@ -1,5 +1,5 @@
 from app.repositories.sensor_node_repository import SensorNodeRepository
-from app.models.sensor_node import SensorNodeDB, SensorNodeOutSlim, TimeseriesData, SensorNodeOutFull, SensorNodeUpdate, SensorNodeCreate, SensorNodeLogbookEntry, SensorNodeLogbookEnum, SensorNodeStateEnum, ConfigurableTypeEnum
+from app.models.sensor_node import SensorNodeDB, SensorNodeOutSlim, ConfigurableAssignment, TimeseriesData, SensorNodeOutFull, SensorNodeUpdate, SensorNodeCreate, SensorNodeLogbookEntry, SensorNodeLogbookEnum, SensorNodeStateEnum, ConfigurableTypeEnum
 from app.models.user import UserInDB, UserOut, RoleEnum
 from app.models.node_template import NodeTemplateStateEnum
 from app.services.project_service import ProjectService
@@ -56,6 +56,10 @@ class SensorNodeService:
         if sorted(user_configs_template) != sorted(user_configs_node):
             raise ValueError("The user-defined configurables do not match the ones defined in the node template.")
         
+        # TODO Replace this block with: Create the sensor node in TTN and get the device ids
+        system_configs = [ConfigurableAssignment(**config.model_dump(), value=str(uuid4())) 
+                          for config in node_template.configurables if config.type == ConfigurableTypeEnum.SYSTEM_DEFINED]
+        
         # Create the sensor node
         uuid = uuid4()
         logbook = [SensorNodeLogbookEntry(type=SensorNodeLogbookEnum.CREATED, date=datetime.now(), user=UserOut(**logged_in_user.model_dump()))]
@@ -66,6 +70,7 @@ class SensorNodeService:
             state=SensorNodeStateEnum.PREPARED, 
             logbook=logbook,
             ttn_device_link=ttn_device_link)
+        sensor_node_db.configurables.extend(system_configs)
         sensor_node_db = self._sensor_node_repository.create_sensor_node(sensor_node_db)
         
         # Update state of the node template
@@ -76,7 +81,45 @@ class SensorNodeService:
         return sensor_node_out
     
     def update_sensor_node(self, uuid: UUID, sensor_node: SensorNodeUpdate, logged_in_user: UserInDB) -> SensorNodeOutFull:
-        pass
+        sensor_node_db = self._sensor_node_repository.find_sensor_node_by_uuid(uuid=uuid)
+        if not sensor_node_db:
+            raise NotFoundError("Sensor node not found")
+        if sensor_node.uuid and sensor_node_db.uuid != sensor_node.uuid:
+            raise ValueError("UUID must not be changed in payload")
+        
+        # If state is still PREPARED, almost all changes should be allowed
+        if sensor_node_db.state == SensorNodeStateEnum.PREPARED:
+            if sensor_node.state != SensorNodeStateEnum.PREPARED:
+                raise ValueError("State of sensore node must not be updated manually")
+            # Check if each configurable is identical to the ones defined in the node template
+            node_configurables_names = [config.name for config in sensor_node_db.configurables]
+            template_configurables_names = [config.name for config in sensor_node.configurables]
+            if sorted(node_configurables_names) != sorted(template_configurables_names):
+                raise ValueError("The configurables do not match the ones defined in the node template.")
+            # check if value of system-defined configurables is not changed
+            for config in sensor_node_db.configurables:
+                if config.type == ConfigurableTypeEnum.SYSTEM_DEFINED:
+                    for config_update in sensor_node.configurables:
+                        if config.name == config_update.name and config.value != config_update.value:
+                            raise ValueError("The value of system-defined configurables cannot be changed")
+            
+            sensor_node_update = SensorNodeDB(
+                **sensor_node.model_dump(), 
+                logbook=sensor_node_db.logbook,
+                project_uuid=sensor_node_db.project_uuid,
+                node_template_uuid=sensor_node_db.node_template_uuid,
+                ttn_device_link=sensor_node_db.ttn_device_link)
+            sensor_node_update.logbook.append(
+                SensorNodeLogbookEntry(
+                    type=SensorNodeLogbookEnum.UPDATED, 
+                    date=datetime.now(), 
+                    user=UserOut(**logged_in_user.model_dump())
+                ))
+            return self._sensor_node_repository.update_sensor_node(sensor_node=sensor_node_update)
+        
+        # TODO Update mechanics when state not PREPARED
+        else:
+            raise ValueError("Sensor node cannot be updated, it is not in the PREPARED state")
     
     def delete_sensor_node(self, uuid: UUID) -> None:
         sensor_node_db = self._sensor_node_repository.find_sensor_node_by_uuid(uuid=uuid)
