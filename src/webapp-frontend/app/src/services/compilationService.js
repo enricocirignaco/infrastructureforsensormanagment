dce6547b26dad98d9e998cea6590fd3f48123f6f
Enrico Cirignaco <enrico.cirignaco@students.bfh.ch>
2025-05-20 12:12:39 +0000

    compilation service created

---
+++ src/webapp-frontend/app/src/services/compilationService.js
@@ -0,0 +1,54 @@
+import { useTextStore } from "@/stores/textStore"
+import { useAuthStore } from "@/stores/authStore"
+const BASE_URL = useTextStore().restApiBaseUrl
+const authStore = useAuthStore()
+
+export default {
+    buildFirmware: function(sensorId) {
+        let url = BASE_URL + '/compilation/build/' + sensorId
+        const options = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                ...authStore.getAuthHeader(),
+                'Content-Type': 'application/json',
+            },
+        }
+        return Promise.resolve({
+            job_id: "123e4567-e89b-12d3-a456-426614174000",
+            status: "pending",
+            message: "Building process started in background",
+            timestamp: "2021-10-10T10:10:10Z"
+        });
+        return fetch(url, options)
+            .then(response => response.ok ? response.json() : Promise.reject(response));
+    },
+    getBuildJobStatus: function(jobId) {
+        let url = BASE_URL + '/compilation/job/' + jobId + '/status'
+        const options = {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                ...authStore.getAuthHeader(),
+            },
+        }
+        return Promise.resolve({
+            "status": "success",
+            "message": "Building process in progress"
+            });
+        return fetch(url, options)
+            .then(response => response.ok ? response.json() : Promise.reject(response));
+    },
+    getBuildArtifact: function(jobId, getBinaryOnly, getSourceCode, getLogs) {
+        let url = BASE_URL + '/compilation/job/' + jobId + '/artifacts?' + getBinaryOnly + '&' + getSourceCode + '&' + getLogs
+        const options = {
+            method: 'GET',
+            headers: {
+                ...authStore.getAuthHeader(),
+            },
+        }
+        return Promise.resolve(new Blob([""], { type: "application/octet-stream" }));
+        return fetch(url, options)
+            .then(response => response.ok ? response : Promise.reject(response));
+    }
+}
